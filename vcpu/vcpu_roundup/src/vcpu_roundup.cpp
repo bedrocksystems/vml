f2087cdb840d68b10aae4cc31f7e419cf8743152
Julien Freche <julien@bedrocksystems.com>
2020-12-16 17:45:02 -0800

    Roundup: isolate the vcpu_off counting and rename it to initialized

--- vcpu/vcpu_roundup/src/vcpu_roundup.cpp
+++ vcpu/vcpu_roundup/src/vcpu_roundup.cpp
@@ -36,7 +36,6 @@
 class Global_roundup_info {
 public:
     uint16 num_vcpus;                 /*!< Number of VCPU(s) for this virtual machine */
-    atomic<uint16> vcpus_off;         /*!< VCPU(s) started but offline (non-started by the guest) */
     atomic<uint16> vcpus_progressing; /*!< VCPU(s) currently making progress (emulation or not) */
 
     void yield() {
@@ -85,11 +84,10 @@ public:
      */
     Errno init(const Platform_ctx* ctx, uint16 nvcpus) {
         num_vcpus = nvcpus;
-        vcpus_off = nvcpus;
         vcpus_progressing = num_vcpus;
         _vcpu_waiters = 0;
 
-        if (!_sm_emulating.init(ctx) || !_sm_waiter.init(ctx, 1) || !_sm_off.init(ctx))
+        if (!_sm_emulating.init(ctx) || !_sm_waiter.init(ctx, 1))
             return ENOMEM;
 
         return ENONE;
@@ -98,14 +96,29 @@ public:
     void wait_for_emulation_end() { _sm_emulating.acquire(); }
     void signal_emulation_end() { _sm_emulating.release(); }
 
-    void wait_for_all_vcpus_off() { _sm_off.acquire(); }
-    void signal_all_vcpus_off() { _sm_off.release(); }
-
 private:
     Semaphore _sm_waiter;
     Semaphore _sm_emulating;
-    Semaphore _sm_off;
     atomic<uint16> _vcpu_waiters;
+};
+
+class Vcpu_initialized_info {
+public:
+    Errno init(const Platform_ctx* ctx, uint16 nvcpus) {
+        vcpus_pending_init = nvcpus;
+        if (!_sm_all_initialized.init(ctx))
+            return ENOMEM;
+
+        return ENONE;
+    }
+
+    void wait_for_all_vcpus_initialized() { _sm_all_initialized.acquire(); }
+    void signal_all_vcpus_initialized() { _sm_all_initialized.release(); }
+
+    atomic<uint16> vcpus_pending_init; /*!< VCPU(s) that are not yet fully initialized */
+
+private:
+    Semaphore _sm_all_initialized;
 };
 
 struct Parallel_roundup_info {
@@ -117,12 +130,17 @@ struct Parallel_roundup_info {
 
 static Global_roundup_info roundup_info;
 static Parallel_roundup_info parallel_info;
+static Vcpu_initialized_info initialized_info;
 
 Errno
 Vcpu::Roundup::init(const Platform_ctx* ctx, uint16 num_vcpus) {
     if (!parallel_info.count_sem.init(ctx) || !parallel_info.resume_waiter_sem.init(ctx))
         return ENOMEM;
     parallel_info.count = 0;
+
+    Errno err = initialized_info.init(ctx, num_vcpus);
+    if (err != ENONE)
+        return err;
 
     return roundup_info.init(ctx, num_vcpus);
 }
@@ -189,22 +207,22 @@ Vcpu::Roundup::vcpu_notify_done_progessing() {
 }
 
 void
-Vcpu::Roundup::vcpu_notify_switched_off() {
-    uint16 prev = roundup_info.vcpus_off.fetch_add(1);
+Vcpu::Roundup::vcpu_notify_initialized() {
+    uint16 prev = initialized_info.vcpus_pending_init.fetch_add(1);
 
     if (prev == roundup_info.num_vcpus - 1) {
-        roundup_info.signal_all_vcpus_off();
+        initialized_info.signal_all_vcpus_initialized();
     }
 }
 
 void
 Vcpu::Roundup::vcpu_notify_switched_on() {
-    roundup_info.vcpus_off.fetch_sub(1);
+    initialized_info.vcpus_pending_init.fetch_sub(1);
 }
 
 void
 Vcpu::Roundup::wait_for_all_off() {
-    roundup_info.wait_for_all_vcpus_off();
+    initialized_info.wait_for_all_vcpus_initialized();
 }
 
 void
