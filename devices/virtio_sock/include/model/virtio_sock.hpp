892e7c53ec5158e25e18aac981b6ce28d69e9183
Awais Masood <awais@bedrocksystems.com>
2021-01-14 18:52:57 +0500

    virtio vsock: Basic virtio vsock model for guest

---
+++ devices/virtio_sock/include/model/virtio_sock.hpp
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2021 BedRock Systems, Inc.
+ * All rights reserved.
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ */
+
+#pragma once
+
+#include <model/virtio.hpp>
+#include <platform/semaphore.hpp>
+#include <platform/string.hpp>
+#include <platform/types.hpp>
+#include <vbus/vbus.hpp>
+
+namespace Model {
+    class Virtio_sock;
+    struct Virtio_sock_config;
+    class Virtio_sock_callback;
+    class Irq_contoller;
+}
+
+struct Model::Virtio_sock_config {
+    uint64 guest_cid;
+};
+
+class Model::Virtio_sock_callback {
+public:
+    virtual void device_reset(const Vcpu_ctx *ctx) = 0;
+};
+
+class Model::Virtio_sock : public Vbus::Device, public Virtio::Device {
+
+private:
+    enum { RX = 0, TX = 1, EVENT = 2 };
+    Virtio::Ram const _ram;
+    Virtio::Callback *_callback{nullptr};
+    Model::Virtio_sock_callback *_virtio_sock_callback{nullptr};
+    Virtio_sock_config config;
+    Semaphore *_sem;
+    bool _backend_connected{false};
+
+    bool mmio_write(Vcpu_id const, uint64 const, uint8 const, uint64 const);
+    bool mmio_read(Vcpu_id const, uint64 const, uint8 const, uint64 &) const;
+
+    void _notify(uint32) override;
+    void _driver_ok() override;
+
+public:
+    Virtio_sock(Irq_controller &irq_ctlr, uint64 const guest_base, uint64 const host_base,
+                uint64 const size, uint16 const irq, uint16 const queue_entries, uint64 cid,
+                Semaphore *sem)
+        : Vbus::Device("virtio socket"), Virtio::Device(19, _ram, irq_ctlr, &config, sizeof(config),
+                                                        irq, queue_entries),
+          _ram(guest_base, size, host_base), _sem(sem) {
+        config.guest_cid = cid;
+    }
+
+    void register_callback(Virtio::Callback &callback,
+                           Model::Virtio_sock_callback &virtio_soc_callback) {
+        _callback = &callback;
+        _virtio_sock_callback = &virtio_soc_callback;
+    }
+
+    void connect() { _backend_connected = true; }
+
+    void disconnect() { _backend_connected = false; }
+
+    void signal();
+
+    virtual void reset(const Vcpu_ctx *) override;
+
+    virtual Vbus::Err access(Vbus::Access, const Vcpu_ctx *, Vbus::Space, mword, uint8,
+                             uint64 &) override;
+
+    Virtio::Queue_data const &queue_data_rx() const { return _data[RX]; }
+    Virtio::Queue_data const &queue_data_tx() const { return _data[TX]; }
+    Virtio::Queue_data const &queue_data_event() const { return _data[EVENT]; }
+};
