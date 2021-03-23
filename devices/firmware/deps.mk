b08d0b7af50e80850a1d067cd9658174fc3b059a
Julien Freche <julien@bedrocksystems.com>
2021-03-23 16:35:19 +0000

    mk: cleanup: remove unused libraries

--- devices/firmware/deps.mk
+++ devices/firmware/deps.mk
@@ -1,7 +1,7 @@
-LIBS = vbus cpu_model gic timer vmm_debug simple_as vcpu vcpu_roundup msr $(PLATFORM)
+LIBS = vbus cpu_model gic timer vmm_debug simple_as vcpu_roundup msr $(PLATFORM)
 
 ifeq ($(PLATFORM), bedrock)
-LIBS += nova zeta pt msc uuid alloc lang cxx log pm_client lifecycle_bedrock concurrent
+LIBS += nova zeta pt msc uuid alloc lang cxx log lifecycle_bedrock concurrent
 else
 # Empty lifecycle, just provides the API
 LIBS += lifecycle
