ec671466962a7e0e99540c6007badfa77ab54de0
Julien Freche <julien@bedrocksystems.com>
2021-04-05 09:48:32 -0700

    Timer: cleanup: use signal instead of semaphore - easier to reason about

--- devices/timer/deps.mk
+++ devices/timer/deps.mk
@@ -1,7 +1,7 @@
 LIBS = lang cxx vbus irq_controller cpu_model $(PLATFORM)
 
 ifeq ($(PLATFORM), bedrock)
-LIBS += lang cxx log zeta nova pt alloc msc uuid
+LIBS += lang cxx log zeta nova pt alloc msc uuid concurrent
 endif
 
 $(eval $(call dep_hook,timer,$(LIBS)))
