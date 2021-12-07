326525788d190aa370e5a26e4e8086213ff3fe51
Mikhail Ershov <mikhail@bedrocksystems.com>
2021-12-07 17:20:43 +0000

    msr: Add OSLSR_EL1 register

--- devices/msr/src/msr.cpp
+++ devices/msr/src/msr.cpp
@@ -476,6 +476,9 @@ Msr::Bus::setup_arch_msr(const Msr::Bus::PlatformInfo &info, Vbus::Bus &vbus, Mo
     reg = new (nothrow) Msr::Register("OSLAR_EL1", OSLAR_EL1, true, 0x0ULL);
     if (!register_system_reg(reg))
         return false;
+    reg = new (nothrow) Msr::Register("OSLSR_EL1", OSLSR_EL1, true, 0x0ULL);
+    if (!register_system_reg(reg))
+        return false;
 
     if (gicd.version() == Model::GIC_V3) {
         if (!setup_gic_registers(gicd))
