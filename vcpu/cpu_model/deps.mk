574422f17e80f23e4c82e50fb4a707f77ec9f07e
Julien Freche <julien@bedrocksystems.com>
2022-03-09 07:53:48 -0800

    VML: clean up LIBDIR and indirect deps

--- vcpu/cpu_model/deps.mk
+++ vcpu/cpu_model/deps.mk
@@ -1,7 +1,3 @@
 LIBS = vbus irq_controller vcpu_roundup vmm_debug $(PLATFORM) arch_api
 
-ifeq ($(PLATFORM), bedrock)
-LIBS += lang cxx log nova zeta pt alloc msc uuid concurrent
-endif
-
 $(eval $(call dep_hook,cpu_model,$(LIBS)))
