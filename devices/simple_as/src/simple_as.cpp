73e3c727d248f2343d02445245c73c7cb2da1a85
Mikhail Ershov <mikhail@bedrocksystems.com>
2022-03-22 14:07:45 +0100

    simple_as: Do not flush read-only host memory in flush_guest_as.
    dc civac can work only with writable memory.

--- devices/simple_as/src/simple_as.cpp
+++ devices/simple_as/src/simple_as.cpp
@@ -193,7 +193,7 @@ Model::SimpleAS::clean_invalidate(GPA gpa, size_t size) const {
 
 void
 Model::SimpleAS::flush_guest_as() {
-    if (_read_only or not _flushable)
+    if (_read_only or not _flushable or not _mobject.cred().write())
         return;
 
     void* mapped_area;
