07dca386aab9532757a031be3d5f27915f3b1e00
Gregory Malecha <gregory@bedrocksystems.com>
2022-04-06 02:26:03 +0000

    Cpu flag

--- vcpu/cpu_model/include/model/cpu.hpp
+++ vcpu/cpu_model/include/model/cpu.hpp
@@ -7,6 +7,7 @@
  */
 #pragma once
 
+#include <model/cpu_feature.hpp>
 #include <model/vcpu_types.hpp>
 #include <platform/atomic.hpp>
 #include <platform/context.hpp>
@@ -20,7 +21,6 @@
 namespace Model {
     class Cpu_irq_interface;
     class Cpu;
-    class Cpu_feature;
     class Irq_controller;
     class Local_Irq_controller;
 }
@@ -33,110 +33,6 @@ class Model::Cpu_irq_interface {
 public:
     virtual void notify_interrupt_pending() = 0;
     virtual Model::Local_Irq_controller *local_irq_ctlr() = 0;
-};
-
-namespace Request {
-    enum Requestor : uint32 {
-        VMM = 0,
-        VMI = 1,
-        MAX_REQUESTORS,
-    };
-}
-
-/**
- * Important note:
- * - Clients that *read* the configuration MUST call `clean`
- *   *before* calling `read`.
- */
-class Model::Cpu_feature {
-public:
-    bool is_requested_by(Request::Requestor requestor) const {
-        ASSERT(requestor < Request::MAX_REQUESTORS);
-        return _requests[requestor] & ENABLE_MASK;
-    }
-    bool is_requested() const {
-        return is_requested_by(Request::VMM) || is_requested_by(Request::VMI);
-    }
-    void force_reconfiguration() { _dirty = true; }
-    bool needs_reconfiguration() const { return _dirty; }
-
-    /**
-     * \brief Reads at the current configuration without committing to
-     *        act on it.
-     */
-    void read(bool &enabled, Reg_selection &regs) const {
-        uint64 conf = _requests[0] | _requests[1];
-
-        enabled = conf & ENABLE_MASK;
-
-        if (enabled)
-            regs = conf & ~ENABLE_MASK;
-        else
-            regs = 0; // Force empty registers when the feature is disabled
-    }
-
-    /**
-     * \brief checks the dirty status and reads if the values are dirty.
-     * This function is provided for convenience.
-     *
-     * This function is marked `nodiscard`, if you do not need the return value,
-     * use `clean_read`.
-     *
-     * \param always if true, read the configuration even if it is not dirty.
-     * \returns true if the configuration has been updated since the last read.
-     */
-    [[nodiscard]] bool check_clean_read(bool &enabled, Reg_selection &regs, bool always = false) {
-        bool dirty = check_clean();
-
-        if (always | dirty)
-            read(enabled, regs);
-
-        return dirty;
-    }
-
-    void clean_read(bool &enabled, Reg_selection &regs) {
-        clean();
-        read(enabled, regs);
-    }
-
-    // Each requestor is responsible of maintaining the consistency of its config
-    void request(bool enable, Request::Requestor requestor, Reg_selection regs = 0) {
-        ASSERT(requestor < Request::MAX_REQUESTORS);
-        ASSERT(!(ENABLE_MASK & regs)); // Reg_selection doesn't use the highest bit for now
-        _requests[requestor] = enable ? regs | ENABLE_MASK : 0;
-        // it is necessary that setting the dirty bits happens after
-        // updating the value.
-        force_reconfiguration();
-    }
-
-    // Low-level interface
-
-    /**
-     * The read of the configuration (through `peek`) *must* be done
-     * *after* this operation.
-     * For a safer interface, use `clean_read`.
-     */
-    [[nodiscard]] bool check_clean() {
-        bool b = _dirty;
-        if (b)
-            // this test is not necessary, it trades a branch for a memory fence
-            _dirty = false;
-        return b;
-        // alternative implementation:
-        //   return _dirty.fetch_and(0);
-        // on ARM, this is a cas-loop which is less efficient.
-        // on x86, this might be more efficient
-    }
-    void clean() { _dirty = false; }
-
-private:
-    static constexpr uint8 ENABLE_SHIFT = 63;
-    static constexpr uint64 ENABLE_MASK = 1ull << ENABLE_SHIFT;
-
-    atomic<uint64> _requests[Request::MAX_REQUESTORS] = {0ull, 0ull};
-    // The `_dirty` bit could use release writes and acquire reads as a more
-    // performant solution than SEQ_CST.
-    atomic<bool> _dirty{false};
 };
 
 class Model::Cpu : public Model::Cpu_irq_interface {
@@ -222,11 +118,11 @@ protected:
     Mode start_mode() const { return _start_mode; }
     uint64 timer_offset() const { return _timer_offset; }
 
-    Cpu_feature _reset;
+    Cpu_flag _reset;
     Cpu_feature _tvm;
-    Cpu_feature _single_step;
-    Cpu_feature _execution_paused;
-    Cpu_feature _icache_invalidate;
+    Cpu_flag _single_step;
+    Cpu_flag _execution_paused;
+    Cpu_flag _icache_invalidate;
     Cpu_feature _hypercall;
 
 public:
