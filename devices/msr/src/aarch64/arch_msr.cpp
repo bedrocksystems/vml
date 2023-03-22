29e8f99afe7959af5e4ae824ffd2334b1708085e
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2023-03-22 21:21:21 +0100

    Quick split for msr lib: skeleton

---
+++ devices/msr/src/aarch64/arch_msr.cpp
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2019-2020 BedRock Systems, Inc.
+ * All rights reserved.
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ */
+
+#include <msr/arch_msr.hpp>
