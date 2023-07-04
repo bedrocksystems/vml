073aa0703cf995e002434ea6e4d42091e95cf50a
Dmitry Malkin <dmitry@bedrocksystems.com>
2023-07-04 11:50:56 +0200

    x86 VMM: add reading from host RTC to CMOS emulation

---
+++ platform/posix/include/platform/rtc.hpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 BedRock Systems, Inc.
+ * All rights reserved.
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ */
+
+#pragma once
+
+#include <platform/errno.hpp>
+
+namespace RTC {
+    struct Date {
+        uint8 seconds;
+        uint8 minutes;
+        uint8 hours;
+        uint8 day_of_week;
+        uint8 day;
+        uint8 month;
+        uint16 year;
+    };
+
+    class Rtc {
+    public:
+        Errno get_date(Date &) { return Errno::NODEV; }
+    };
+};
