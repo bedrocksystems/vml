46bf0b9e52ebdd85a92c87feffa2206f21fea05b
Jonas Pfoh <jonas@bedrocksystems.com>
2024-01-23 11:44:42 +0100

    x86 VMM: move cpu consts into seperate header

---
+++ vcpu/cpu_model/include/x86/model/consts.hpp
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2024 BedRock Systems, Inc.
+ * All rights reserved.
+ *
+ * This software is distributed under the terms of the BedRock Open-Source License.
+ * See the LICENSE-BedRock file in the repository root for details.
+ */
+#pragma once
+
+#include <bedrock/types.hpp>
+
+static constexpr uint32 PROC_CNTR0_CR3_LOAD = 1u << 15;
+static constexpr uint32 PROC_CNTR0_TPR_SHADOW = 1u << 21;
+static constexpr uint32 PROC_CNTR0_MTF = 1u << 27;
+
+static constexpr uint32 PROC_CNTR1_VIRT_APIC_ACCESS = 1u << 0;
+static constexpr uint32 PROC_CNTR1_IDT_GDT = 1u << 2;
+static constexpr uint32 PROC_CNTR1_ENABLE_RDTSCP = 1u << 3;
+static constexpr uint32 PROC_CNTR1_VIRT_X2APIC_MODE = 1u << 4;
+static constexpr uint32 PROC_CNTR1_APIC_REG_VIRT = 1u << 8;
+static constexpr uint32 PROC_CNTR1_VIRT_IRQ_DELIVERY = 1u << 9;
+static constexpr uint32 PROC_CNTR1_ENABLE_INVPCID = 1u << 12;
+static constexpr uint32 PROC_CNTR1_VIRT_XSAVES = 1u << 20;
+static constexpr uint32 PROC_CNTR1_MBEC = 1u << 22;
