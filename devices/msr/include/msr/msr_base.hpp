1aa071589b4358c4bd4fa66740f0a78deeb04edb
Irfan Ulla Khan <irfan.khan@bedrocksystems.com>
2024-04-02 17:33:45 +0000

    Vcpus MSR stats

--- devices/msr/include/msr/msr_base.hpp
+++ devices/msr/include/msr/msr_base.hpp
@@ -9,16 +9,26 @@
 
 #include <intrusive/map.hpp>
 #include <msr/msr_id.hpp>
+#include <platform/time.hpp>
 #include <vbus/vbus.hpp>
 
 namespace Msr {
 
     class RegisterBase;
     class Register;
+    class BusStats;
     class BaseBus;
 
     using Vbus::Err;
 }
+
+struct MsrStats {
+    uint64 read_count{0ull};
+    uint64 write_count{0ull};
+    Tsc min{~0ull};
+    Tsc max{0ull};
+    Tsc total{0ull};
+};
 
 class Msr::RegisterBase : public Map_key<mword> {
 public:
@@ -43,9 +53,41 @@ public:
      */
     const char* name() const { return _name; }
 
+    Tsc msr_stats_start(Vbus::Access access) {
+        if (access == Vbus::WRITE)
+            _msr_stats.write_count++;
+        if (access == Vbus::READ)
+            _msr_stats.read_count++;
+
+        return static_cast<Tsc>(clock());
+    }
+
+    void msr_stats_end(Tsc t) {
+        Tsc time_spent = static_cast<Tsc>(clock()) - t;
+        _msr_stats.total += time_spent;
+
+        if (time_spent < _msr_stats.min)
+            _msr_stats.min = time_spent;
+
+        if (time_spent > _msr_stats.max)
+            _msr_stats.max = time_spent;
+    }
+
+    MsrStats* get_stats() { return &_msr_stats; }
+
+    void reset_stats() {
+        _msr_stats.read_count = 0;
+        _msr_stats.write_count = 0;
+        _msr_stats.min = ~0ull;
+        _msr_stats.max = 0;
+        _msr_stats.total = 0;
+    }
+
 private:
     const char* _name; /*!< Name of the device - cannot be changed at run-time */
     Id _reg_id;
+
+    MsrStats _msr_stats;
 };
 
 class Msr::Register : public RegisterBase {
@@ -78,13 +120,28 @@ public:
     void reset(const VcpuCtx*) override { _value = _reset_value; }
 };
 
+class Msr::BusStats {
+public:
+    explicit BusStats(Map_kv<mword, RegisterBase>* devs) : _devices(devs) {}
+
+    uint64 total_access{0ull};
+    Tsc last_seen{0ull};
+    const RegisterBase* last_access{nullptr};
+
+    Map_kv<mword, RegisterBase>::iterator begin() const { return _devices->begin(); }
+    Map_kv<mword, RegisterBase>::iterator end() const { return _devices->end(); }
+
+private:
+    Map_kv<mword, RegisterBase>* const _devices;
+};
+
 /*
  * This is the bus that will handle all reads and writes
  * to system registers.
  */
 class Msr::BaseBus {
 public:
-    BaseBus() {}
+    BaseBus() : _msrs_stats(&_devices) {}
 
     /*! \brief Add a register to the msr bus
      *  \pre Full ownership of a valid virtual bus. Full ownership of a valid register.
@@ -134,7 +191,14 @@ public:
         _fold = fold_successive;
     }
 
+    /*! \brief Debug only: control the msr stats collection
+     *  \param enabled collect msr stats?
+     */
+    void set_stats(bool enabled) { _stats_enabled = enabled; }
+
     RegisterBase* get_register_with_id(Msr::Id id) const { return reinterpret_cast<RegisterBase*>(get_device_at(id.id())); }
+
+    const BusStats* get_stats_ptr() { return &_msrs_stats; }
 
 private:
     static void reset_register_cb(Msr::RegisterBase*, const VcpuCtx*);
@@ -142,10 +206,15 @@ private:
     void log_trace_info(const RegisterBase* reg, Vbus::Access access, uint64 val);
 
     Map_kv<mword, RegisterBase> _devices;
+
     bool _trace{false};
     bool _fold{true};
+
     const RegisterBase* _last_access{nullptr};
     size_t _num_accesses{0};
+
+    BusStats _msrs_stats;
+    bool _stats_enabled{false};
 
 protected:
     bool register_system_reg(RegisterBase* reg) {
