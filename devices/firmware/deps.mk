f7f4d0f18d31a2904c3c3c405d4b13f5144a5b7a
Julien Freche <julien@bedrocksystems.com>
2024-08-06 15:52:26 -0700

    VMM, PLATFORM: rename the last libs with bedrock in the name

--- devices/firmware/deps.mk
+++ devices/firmware/deps.mk
@@ -1,7 +1,7 @@
 LIBS = cpu_model simple_as vcpu_roundup msr
 
-ifeq ($(PLATFORM), bedrock)
-LIBS += lifecycle_bedrock bedrock
+ifeq ($(PLATFORM), bluerock)
+LIBS += lifecycle_bluerock bluerock
 else
 # Empty lifecycle, just provides the API
 LIBS += lifecycle
